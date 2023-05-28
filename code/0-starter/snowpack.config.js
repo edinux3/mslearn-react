1bdee75f8a3021156d66ea67adb55231a5a71a97
edinux3 <edinux10@gmail.com>
2023-05-28 18:23:20 +0200

    Practica Microsoft

--- code/0-starter/snowpack.config.js
+++ code/0-starter/snowpack.config.js
@@ -1,6 +1,3 @@
-module.exports = {
-    mount: {
-        'public': '/',
-        'src': '/dist'
-    }
-}
+import React from "../web_modules/react.js";
+import ReactDOM from "../web_modules/react-dom.js";
+ReactDOM.render(/* @__PURE__ */ React.createElement(React.createElement("h1", null, "Hello, world!")), document.getElementById("app"));
